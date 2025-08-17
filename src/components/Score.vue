4ac6bb83a71b89725b48a9c0289ce7a3b6888891
Adam <aburabu@mail.ru>
2025-08-17 12:39:07 +0300

    homework-4

---
+++ src/components/Score.vue
@@ -0,0 +1,30 @@
+<script setup>
+    import IconScore from '../icons/IconScore.vue';
+
+    const {score} = defineProps({
+        score: Number,
+    })
+
+</script>
+
+<template>
+    <div class="score">
+        <div class="score-stat">{{ score }}</div>
+        <IconScore/>
+    </div>
+</template>
+
+<style scoped>
+    .score {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        column-gap: 10px;
+        color: var(--color-primary);
+        border-radius: 36px;
+        background-color: var(--color-primary-accent);
+        width: 100%;
+        max-width: 110px;
+        height: 48px;
+    }
+</style>
---
+++ src/icons/IconFail.vue
@@ -0,0 +1,11 @@
+<script setup></script>
+
+<template>
+   <svg viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="48.000000" height="48.000000" fill="none" customFrame="#000000">
+        <rect id="Iconly/Sharp/Bold/Close Square" width="48.000000" height="48.000000" x="0.000000" y="0.000000" fill="rgb(255,255,255)" fill-opacity="0" />
+        <path id="Fill 300" d="M30.922 28.778L28.8 30.9L23.998 26.102L19.198 30.894L17.078 28.772L21.876 23.982L17.078 19.186L19.2 17.064L24 21.862L28.802 17.068L30.922 19.192L26.122 23.982L30.922 28.778ZM24 4.5C13.248 4.5 4.5 13.248 4.5 24C4.5 34.752 13.248 43.5 24 43.5C34.752 43.5 43.5 34.752 43.5 24C43.5 13.248 34.752 4.5 24 4.5Z" fill="rgb(207.762,2.78587,2.78587)" fill-rule="evenodd" />
+    </svg>
+
+</template>
+
+<style scoped></style>
